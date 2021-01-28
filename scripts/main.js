97163cf54ae97ea02e6c3934690e5c106098ac3a
fimoefive <brothermj@hotmail.com>
2021-01-28 12:27:40 -0600

    Js Level 1 - I Can Function

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 console.log("Repo-5");
+
+function printPersonalM(name, color) {
+  console.log(`Hey, ${name}, I see you like ${color}.`);
+}
+// printPersonalM("Mr.Sisk", "blue");
+
+var myName = "Dr. Feel Good!";
+var favColor = "red";
+
+printPersonalM(myName, favColor);
